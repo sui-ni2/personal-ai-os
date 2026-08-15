1df93cb9cf76037ef62a63034e5ca5e4e1274265
sui-ni2 <loxri127@gmail.com>
2026-08-15 16:27:37 +0800

    feat(providers): export GitHub Models adapter

--- packages/providers/src/personal_ai_os_providers/__init__.py
+++ packages/providers/src/personal_ai_os_providers/__init__.py
@@ -10,11 +10,13 @@ from .base import (
     ProviderTool,
     ProviderToolCall,
 )
+from .github_models import GitHubModelsAdapter
 from .openai import OpenAIAdapter
 from .registry import ProviderRegistry
 
 __all__ = [
     "AnthropicAdapter",
+    "GitHubModelsAdapter",
     "OpenAIAdapter",
     "ProviderAdapter",
     "ProviderCancelled",
